4981f2e4e415168e808eded6540a4408f8ba743e
tieTYT <tieTYT@gmail.com>
2014-11-24 22:10:07 -0800

    Now goes from 1 to 100

--- src/main/java/yesiknowfizzbuzz/FizzBuzz.java
+++ src/main/java/yesiknowfizzbuzz/FizzBuzz.java
@@ -3,7 +3,7 @@ package yesiknowfizzbuzz;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        for (Integer value = 0; value < 100; value++) {
+        for (Integer value = 1; value <= 100; value++) {
             System.out.print(value + ": ");
             if (value % 3 == 0) {
                 System.out.print("Fizz");
