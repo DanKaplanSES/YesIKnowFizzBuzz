13492c1c8089d94128475271ea12dc171f34012d
tieTYT <tieTYT@gmail.com>
2014-11-24 17:05:45 -0800

    minor refactoring

--- src/main/java/yesiknowfizzbuzz/FizzBuzz.java
+++ src/main/java/yesiknowfizzbuzz/FizzBuzz.java
@@ -3,16 +3,16 @@ package yesiknowfizzbuzz;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        for (Integer element = 0; element < 100; element++) {
-            System.out.print("element " + element + ": ");
-            if (element % 3 == 0) {
+        for (Integer value = 0; value < 100; value++) {
+            System.out.print(value + ": ");
+            if (value % 3 == 0) {
                 System.out.print("Fizz");
             }
-            if (element % 5 == 0) {
+            if (value % 5 == 0) {
                 System.out.print("Buzz");
             }
-            if (element % 3 != 0 && element % 5 != 0) {
-                System.out.print(element);
+            if (value % 3 != 0 && value % 5 != 0) {
+                System.out.print(value);
             }
             System.out.println();
         }
